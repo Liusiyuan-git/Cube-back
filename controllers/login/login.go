7616bfd62b5b5e11282aa114587cae1a362d8c67
liusiyuan <945212191@qq.com>
2021-05-18 22:31:19 +0800

    Cube-back 2021/5/18

---
+++ controllers/login/login.go
@@ -0,0 +1,53 @@
+package login
+
+import (
+	"Cube-back/controllers"
+	"Cube-back/log"
+	"Cube-back/models/user/login"
+)
+
+type Controller struct {
+	controllers.MainController
+}
+
+func (c *Controller) Prepare() {
+	data := c.RequestBodyData()
+	mode := data["mode"]
+	if mode == "" {
+		session := c.GetSession("CubeId")
+		if session == nil {
+			result := make(map[string]interface{})
+			result["msg"] = "请先登录"
+			c.DataCallBack(result, false)
+		}
+	}
+}
+
+var L = new(login.Login)
+
+func (c *Controller) UserLogin() {
+	data := c.RequestBodyData()
+	mode := data["mode"]
+	cubeId := ""
+	msg := ""
+	pass := true
+	if mode == "count" {
+		count := data["count"]
+		password := data["password"]
+		cubeId, msg, pass = L.LoginCount(count, password)
+	}
+	if mode == "phone" {
+		phone := data["phone"]
+		code := data["code"]
+		msg, pass = L.LoginPhone(phone, code)
+	}
+	if pass {
+		sessionErr := c.SetSession("CubeId", cubeId)
+		if sessionErr != nil {
+			log.Error(sessionErr)
+		}
+	}
+	result := make(map[string]interface{})
+	result["msg"] = msg
+	c.DataCallBack(result, pass)
+}
