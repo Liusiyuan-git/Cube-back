5072b4d49cda4d7fedca166c2be153c4241eb581
liusiyuan <945212191@qq.com>
2021-06-22 21:51:15 +0800

    Cube-back 2021/6/22

--- models/models.go
+++ models/models.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"Cube-back/models/blog"
+	"Cube-back/models/draft"
 	"Cube-back/models/user"
 	"github.com/beego/beego/v2/client/orm"
 	"github.com/beego/beego/v2/core/logs"
@@ -8,6 +10,8 @@ import (
 
 func ModelRegister() {
 	orm.RegisterModel(new(user.User))
+	orm.RegisterModel(new(blog.Blog))
+	orm.RegisterModel(new(draft.Draft))
 }
 
 func RunSyncdb() {
